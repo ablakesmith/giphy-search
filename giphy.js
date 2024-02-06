b5a0b9dfdd1df3113610064bfb87a63e1e888d03
ablakesmith <a.blake.smith11@gmail.com>
2024-02-05 19:22:13 -0500

    giphysearch

---
+++ giphy.js
@@ -0,0 +1,32 @@
+function formSubmitted(event) {
+    event.preventDefault();
+    const memeText = document.querySelector("#memeTextInput").value;
+    const memeCount = Number.parseInt(document.querySelector("#numOfGifsSelect").value);
+
+    getData(memeText, memeCount);
+
+    document.querySelector("#memeTextInput").value = "";
+}
+
+const API_KEY = "5RgrrJTJyFlKFVO456dz0RmUR6qVrr6m";
+
+function getData(memeText, memeCount) {
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${memeText}&limit=${memeCount}`)
+        .then((x) => x.json())
+        .then(renderData);
+}
+
+function renderData(response) {
+    let html = "";
+    for (let image of response.data) {
+        html += `
+            <img 
+                src="${image.images.original.url} 
+                alt="${image.title}" 
+                class="giphy-img" />
+        `;
+    }
+    document.querySelector(".js-images").innerHTML = html;
+}
+
+document.querySelector(".js-giphy-form").addEventListener("submit", formSubmitted);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Giphy Search App
+
+**GitHub Pages Link:** [absmith-portfolio on GitHub Pages](https://ablakesmith.github.io/absmith-portfolio/)
